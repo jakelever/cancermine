d777f0684672619c36e2faf8b6c718167eef94a5
Jake Lever <jlever@bcgsc.ca>
2017-12-06 12:59:03 -0800

    Build model for each relation type

--- buildModel.py
+++ buildModel.py
@@ -6,22 +6,32 @@ import os
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Build and save a classifier')
 	parser.add_argument('--inTrain',type=str,required=True)
-	parser.add_argument('--outModel',type=str,required=True)
+	parser.add_argument('--outModel_Driver',type=str,required=True)
+	parser.add_argument('--outModel_Oncogene',type=str,required=True)
+	parser.add_argument('--outModel_TumorSuppressor',type=str,required=True)
 
 	args = parser.parse_args()
 
-	print("Loading training")
-	goldDir = 'gold'
-	trainCorpus = kindred.loadDir(dataFormat='standoff',directory=args.inTrain)
+	thresholds = {'Driver':0.89, 'Oncogene': 0.94, 'Tumor_Suppressor': 0.97}
 
-	print("Doing training")
-	features = "entityTypes,unigramsBetweenEntities,bigrams,dependencyPathEdges,dependencyPathEdgesNearEntities".split(',')
-	classifier = kindred.RelationClassifier(classifierType='LogisticRegression',threshold=0.8,features=features,acceptedEntityPairs=[('cancer','gene')])
-	classifier.train(trainCorpus)
+	for relationType,outModel in zip(['Driver','Oncogene','Tumor_Suppressor'], [args.outModel_Driver,args.outModel_Oncogene,args.outModel_TumorSuppressor] ):
+		print("Building %s model" % relationType)
+		print("  Loading training")
+		goldDir = 'gold'
+		trainCorpus = kindred.loadDir(dataFormat='standoff',directory=args.inTrain)
+	
+		for doc in trainCorpus.documents:
+			doc.relations = [ r for r in doc.relations if r.relationType == relationType ]
 
-	print("Saving classifer")
-	with open(args.outModel,'wb') as f:
-		pickle.dump(classifier,f)
+		print("  Doing training")
+		features = "entityTypes,unigramsBetweenEntities,bigrams,dependencyPathEdges,dependencyPathEdgesNearEntities".split(',')
+		threshold = thresholds[relationType]
+		classifier = kindred.RelationClassifier(classifierType='LogisticRegression',threshold=threshold,features=features,acceptedEntityPairs=[('cancer','gene')])
+		classifier.train(trainCorpus)
 
-	print("Done!")
+		print("  Saving classifer")
+		with open(outModel,'wb') as f:
+			pickle.dump(classifier,f)
+
+		print("  Output done!")
 
