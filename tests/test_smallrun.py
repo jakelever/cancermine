dff7be32a3b492403ac11cccf76e9226a22b2800
Jake Lever <jlever@bcgsc.ca>
2017-12-18 19:13:27 -0800

    Small change to defunct test

--- tests/test_smallrun.py
+++ tests/test_smallrun.py
@@ -14,10 +14,12 @@ def test_basicrun():
 	inModel_TumorSuppressor = 'cancermine.tumorsuppressor.model'
 	filterTerms = 'filterTerms.txt'
 	wordlistPickle = 'terms.pickle'
+	genes = 'terms_genes.txt'
+	cancerTypes = 'terms_cancer.txt'
 	outData = 'tests/testDoc.bioc.out'
 
 	os.chdir(parentDir)
-	applyModel.cancermine(biocFile,inModel_Driver,inModel_Oncogene,inModel_TumorSuppressor,filterTerms,wordlistPickle,outData)
+	applyModel.cancermine(biocFile,inModel_Driver,inModel_Oncogene,inModel_TumorSuppressor,filterTerms,wordlistPickle,genes,cancerTypes,outData)
 
 if __name__ == '__main__':
 	test_basicrun()
