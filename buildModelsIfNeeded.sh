e767294f2454e750e0b01acffc0cda219705a006
Jake Lever <jlever@bcgsc.ca>
2018-06-26 18:03:36 -0700

    Build the models if needed by unzipped the cancermine_corpus

--- buildModelsIfNeeded.sh
+++ buildModelsIfNeeded.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -eux
 
 if [ ! -d models ]; then
 	mkdir models
-	unzip data/cancermine_corpus.zip
+	unzip -qq data/cancermine_corpus.zip
+	mv cancermine_corpus/train/ cancermine_corpus/combined/
+	mv cancermine_corpus/test/* cancermine_corpus/combined/
+
 	python buildModels.py --inTrain cancermine_corpus/combined/ --outModel_Driver model/cancermine.driver.model --outModel_Oncogene model/cancermine.oncogene.model --outModel_TumorSuppressor model/cancermine.tumorsuppressor.model
 fi
 
