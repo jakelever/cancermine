ca7d5c3082587a9d018d376554bf3fa4f1dcec2f
Jake Lever <jlever@bcgsc.ca>
2018-08-12 04:17:36 -0700

    Add commands to do demo run

---
+++ demoRun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eux
+
+python parseAndFindEntities.py --biocFile exampledata/input.bioc.xml --filterTerms filterTerms.txt --wordlistPickle exampledata/mini_terms.pickle --outSentencesFilename exampledata/intermediate_sentences.json
+
+python applyModelsToSentences.py --models models/cancermine.driver.model,models/cancermine.oncogene.model,models/cancermine.tumorsuppressor.model --filterTerms filterTerms.txt --wordlistPickle exampledata/mini_terms.pickle --genes exampledata/mini_terms_genes.tsv --cancerTypes exampledata/mini_terms_cancers.tsv --sentenceFile exampledata/intermediate_sentences.json --outData exampledata/intermediate_relations.json
+
+cat header.tsv exampledata/intermediate_relations.json > exampledata/out_unfiltered.tsv
+
+python filterAndCollate.py --inUnfiltered exampledata/out_unfiltered.tsv --outCollated exampledata/out_collated.tsv --outSentences exampledata/out_sentences.tsv
+
